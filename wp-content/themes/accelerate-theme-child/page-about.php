706a44cb9ab813fa0f841ff7244e1dcf393165c7
aepriebebrown <aepriebe@gmail.com>
2016-08-04 05:39:44 -0500

    Added about page and styles.

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -1,28 +1,32 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
+ * The template for displaying the About page.
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
- * @since Accelerate Marketing 1.0
+ * @since Accelerate Marketing 1.1
  */
 
 get_header(); ?>
 
-	<section class="about-page">
-		<div class="about-content">
+	<section class="about-page-header">
+		<div class="about-header">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<div class='homepage-hero'>
-					<?php the_content(); ?>
-					<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
+				<div class='about-hero'>
+					<p><span class="green">Accelerate</span> is a strategy and marketing agency located in the heart of NYC. Our goal is to build businesses by making our clients visible and making their customers smile.</p>
 				</div>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .container -->
-	</section><!-- .home-page -->
+	</section><!-- .about-page-header -->
+	<section class="about-list">
+		<div class="about-content">
+			<ul class="about-featured-services">
+				<?php while ( have_posts() ) : the_post();?>
+					<?php the_content(); ?>
+				<?php endwhile; // end of the loop. ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+			</ul>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
